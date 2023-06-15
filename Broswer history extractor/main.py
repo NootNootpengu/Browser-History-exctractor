0328a63dbdd97b4c8cd190da95d23b75d0023768
Noot Noot <95957330+NootNootpengu@users.noreply.github.com>
2023-06-15 18:08:44 +0330

    Added Main files

---
+++ Broswer history extractor/main.py
@@ -0,0 +1,9 @@
+from browser_history.browsers import Firefox
+
+f = Firefox()
+outputs = f.fetch_history()
+
+# his is a list of (datetime.datetime, url) tuples
+his = outputs.histories
+print(his)
+input()
